774bb2cd90849dc22a885a5e45b7c71fd0eb70fb
Damiano Deidda <damiano.deidda1@gmail.com>
2024-06-25 14:43:56 +0200

    chore: first commit

---
+++ app/git/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Git = () => {
+  return <div>Git</div>;
+};
+
+export default Git;
---
+++ app/steps/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Steps = () => {
+  return (
+    <div className="flex flex-col p-20 w-100 justify-center items-center gap-10">
+      Steps
+    </div>
+  );
+};
+
+export default Steps;
