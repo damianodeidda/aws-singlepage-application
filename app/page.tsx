b3fd247b7572f1742d0504fc84a222d9df700c8a
Damiano Deidda <damiano.deidda1@gmail.com>
2024-06-26 16:08:11 +0200

    style: fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col p-20 w-100 justify-center items-center gap-10">
-      <div className="w-100">
+      <h1 className="white">NextJs app deployed to Aws</h1>
+      {/*   <div className="w-100">
         <Image src={awsLogo} alt="logoAws" width={260} />
-      </div>
+      </div> */}
       <div className="flex flex-row gap-5">
         <Link href={"/steps"}>
           <button className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Steps route
-          </button>
-        </Link>
-        <Link href={"/git"}>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Git
+            Step route
           </button>
         </Link>
       </div>
--- app/steps/page.tsx
+++ app/steps/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Steps = () => {
   return (
     <div className="flex flex-col p-20 w-100 justify-center items-center gap-10">
-      Steps
+      <Link href={"/"}>
+        <button className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Back to the homepage
+        </button>
+      </Link>
     </div>
   );
 };
