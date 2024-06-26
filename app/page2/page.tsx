36f669e52dea6783aee74a74aed17904e1cb7015
Damiano Deidda <damiano.deidda1@gmail.com>
2024-06-26 16:36:54 +0200

    style: fix text

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Page2 = () => {
   return (
     <div className="flex flex-col p-20 w-100 justify-center items-center gap-10">
-      <h1 className="white">This is the page 2</h1>
+      <p className="white">This is the page 2</p>
       <Link href={"/"}>
         <button className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Back to the homepage
